987e8ba96c3d29dbf7eccc8b2fac07726868c9fe
lonewanderer27 <adrianejames27@gmail.com>
2024-10-05 18:15:55 +0800

    Update astro.config.mjs with site and base configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://lonewanderer27.github.io",
+  base: "resume",
   vite: {
     css:{
       preprocessorOptions: {
